f8ed59e6d30241eff48b7e54a7899bd42ddc6be2
Yeicor <4929005+yeicor@users.noreply.github.com>
2025-07-04 23:54:18 +0200

    Fix selector.py

--- cibuildwheel/selector.py
+++ cibuildwheel/selector.py
@@ -99,10 +99,10 @@ class BuildSelector:
             return False
         if EnableGroup.GraalPy not in self.enable and fnmatch(build_id, "gp*"):
             return False
-        #if EnableGroup.PyodidePrerelease not in self.enable and fnmatch(
-        #    build_id, "cp313-pyodide_*"
-        #):
-        #    return False
+        # if EnableGroup.PyodidePrerelease not in self.enable and fnmatch(
+        #     build_id, "cp313-pyodide_*"
+        # ):
+        #     return False
 
         should_build = selector_matches(self.build_config, build_id)
         should_skip = selector_matches(self.skip_config, build_id)
